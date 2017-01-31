d22c6975c39217a6d9058ba4ac05752142c8cf9b
ericmaxwell2003 <ericmaxwell2003@yahoo.com>
2017-01-31 15:08:18 -0500

    Tidy up imports, spacing, and add Realm copywright header to the ShakeSensorEventListener.

--- Android/app/src/main/java/io/realm/draw/sensor/ShakeSensorEventListener.java
+++ Android/app/src/main/java/io/realm/draw/sensor/ShakeSensorEventListener.java
@@ -1,13 +1,25 @@
+/*
+ * Copyright 2016 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.realm.draw.sensor;
 
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-
-/**
- * ShakeSensorEvent Listener adapted from Jason McReynold's post http://jasonmcreynolds.com/?p=388
- */
 
 public class ShakeSensorEventListener implements SensorEventListener {
 
@@ -49,6 +61,7 @@ public class ShakeSensorEventListener implements SensorEventListener {
 
             if (gForce > SHAKE_THRESHOLD_GRAVITY) {
                 final long now = System.currentTimeMillis();
+
                 // ignore shake events too close to each other (500ms)
                 if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
                     return;
