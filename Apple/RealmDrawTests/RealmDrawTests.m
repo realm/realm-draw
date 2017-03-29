61c632560fa481871e9e2cfd1821eccd36618d20
Tim Oliver <me@timoliver.com.au>
2017-03-29 16:52:30 -0700

    Added unit test components

---
+++ Apple/RealmDrawTests/RealmDrawTests.m
@@ -0,0 +1,21 @@
+//
+//  RealmDrawTests.m
+//  RealmDrawTests
+//
+//  Created by Tim Oliver on 3/29/17.
+//  Copyright © 2017 Realm. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RealmDrawTests : XCTestCase
+
+@end
+
+@implementation RealmDrawTests
+
+- (void)testExample {
+    
+}
+
+@end
